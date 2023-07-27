5ec54328b27742e348ad7a9bba44d72dc8440c21
dev-ggaurav <devggaurav@gmail.com>
2023-07-27 20:51:25 +0530

    deployment workflow

--- deploy.sh
+++ deploy.sh
@@ -20,10 +20,10 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://dev-ggaurav.github.io
-# git push -f git@github.com:dev-ggaurav/dev-ggaurav.github.io.git main
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://dev-ggaurav.github.io/react-movies-app
-# git push -f git@github.com:dev-ggaurav/react-movies-app.git main:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+ git push -f git@github.com:dev-ggaurav/react-movies-app.git main:gh-pages
 
 cd -
